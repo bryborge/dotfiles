51c2bb413786229218ce242e59aaa4da09952c28
Bryan Borgeson <bryborge@gmail.com>
2024-10-11 15:59:55 -0700

    Clean up

--- README.md
+++ README.md
@@ -1,33 +1,15 @@
-# Linux Dotfiles
+# Dotfiles
 
-My linux config files.
+All my config files in one place.
 
-## Description
-
-I don't want to have to think about keeping track of multiple versions of my config files.
-So,
-git version control is the perfect solution because:
-
-1.  I have all my configs in one place
-1.  I can easily rollback if shit gets hairy
-
-This works by generating symlinks in the home directory to the config files in this repo.
-If that doesn't makes sense,
-just trust that it's genius.
-
-## How to Use
-
-### Get Started
+## Get Started
 
 1.  Clone this repo down to your code directory: `/home/username/code/`
-1.  Look at the contents of `scripts/gen_symlinks.sh` **BEFORE** running it. Don't be a n00b.
-1.  Run `$ bash scripts/gen_symlinks.sh`
-1.  Bask in the glory
+2.  Look at the contents of `scripts/gen_symlinks.sh` **BEFORE** running it.
+3.  Run `bash scripts/gen_symlinks.sh`
 
 ### Tracking New Files
 
-Currently the process is:
-
 1.  Create the new file in the root directory of this project
-1.  Add the file to the `start()` method in `scripts/gen_symlinks`
-1.  Run `$ bash scripts/gen_symlinks`
+2.  Add the file to the `start()` method in `scripts/gen_symlinks`
+3.  Run `$ bash scripts/gen_symlinks`
--- scripts/gen_symlinks.sh
+++ scripts/gen_symlinks.sh
@@ -2,77 +2,8 @@
 
 set -euo pipefail
 
-##
-# Prompt user to remove all that config backup bullshit.
-cleanup_aisle_five() {
-  echo "    Want to remove your backups now?"
-  read -p "        Enter [y/n]: " opt
-
-  if [[ "y" == $opt ]]; then
-    rm -rf ~/config_bak
-  else
-    printf "\n"
-    echo "    Cool beans. You're all set. \o/"
-  fi
-}
-
-##
-# Create ~/config_bak dir if it doesn't already exist, 'cause we may need that
-# shit.
-create_config_bak() {
-  if [ ! -d ~/config_bak ]; then
-    mkdir ~/config_bak
-  fi
-}
-
-##
-# Lastly, do the mothafuckin' thing!
-ftw() {
-  ln -s ~/code/dotfiles-linux/$1 ~/$1
-  echo "    \"~/$1\" symlink created!"
-}
-
-##
-# Second, actually backup, destroy, or ignore that mothafucka and let user know
-# what the fuck they just did.
-cross_your_tease() {
-  if [[ "i" == $1 ]]; then
-    echo "    Ignoring. Proceed ..."
-  elif [[ "b" == $1 ]]; then
-    create_config_bak
-    mv ~/$2 ~/config_bak
-    echo "    backup created in \"~/config_bak/$2\""
-  elif [[ "d" == $1 ]]; then
-    rm ~/$2
-    echo "    \"~/$2\" was destroyed."
-  else
-    echo "    Fuck you, n00b.  Doing nothing about \"~/$2\""
-  fi
-}
-
-
-##
-# First, life is about options. Check if user wants to backup, destroy or ignore
-# that mothafucka.
-dot_your_eyes() {
-  printf "\n"
-
-  if [[ -f ~/$1 || -d ~/$1 || -L ~/$1 ]]; then
-    echo "    \"~/$1\" already exists! Backup (b), Destroy (d), or Ignore (i)?"
-    read -p "        Enter [b/d/i] : " opt
-    printf "\n"
-    cross_your_tease $opt $1
-    ftw $1
-  else
-    ftw $1
-  fi
-}
-
-
-##
-# Do the mothafuckin' magic ...
-#
-start() {
+# Main function
+main() {
   dot_your_eyes .bash_aliases
   dot_your_eyes .bashrc
   dot_your_eyes .gitcompletion
@@ -87,4 +18,62 @@ start() {
   cleanup_aisle_five
 }
 
-start
+# 1) Ask if user wants to backup, destroy or ignore target.
+dot_your_eyes() {
+  printf "\n"
+
+  if [[ -f ~/$1 || -d ~/$1 || -L ~/$1 ]]; then
+    echo "    \"~/$1\" already exists! Backup (b), Destroy (d), or Ignore (i)?"
+    read -p "        Enter [b/d/i] : " opt
+    printf "\n"
+    cross_your_tease $opt $1
+    ftw $1
+  else
+    ftw $1
+  fi
+}
+
+# 2a) Handle request from the user on the target.
+cross_your_tease() {
+  if [[ "i" == $1 ]]; then
+    echo "    Ignoring. Proceed ..."
+  elif [[ "b" == $1 ]]; then
+    create_config_bak
+    mv ~/$2 ~/config_bak
+    echo "    backup created in \"~/config_bak/$2\""
+  elif [[ "d" == $1 ]]; then
+    rm ~/$2
+    echo "    \"~/$2\" was destroyed."
+  else
+    echo "    Doing nothing. Proceed ... \"~/$2\""
+  fi
+}
+
+# 2b) Create symlink for target.
+ftw() {
+  ln -s ~/code/dotfiles-linux/$1 ~/$1
+  echo "    \"~/$1\" symlink created!"
+}
+
+# 3) Create ~/config_bak dir if it doesn't already exist.
+create_config_bak() {
+  if [ ! -d ~/config_bak ]; then
+    mkdir ~/config_bak
+  fi
+}
+
+# 4) Prompt user to remove all the config backup from preceeding step(s).
+cleanup_aisle_five() {
+  echo "    Want to remove your backups now?"
+  read -p "        Enter [y/n]: " opt
+
+  if [[ "y" == $opt ]]; then
+    rm -rf ~/config_bak
+  else
+    printf "\n"
+    echo "    Cool beans. You're all set. \o/"
+  fi
+}
+
+# Do it.
+main
